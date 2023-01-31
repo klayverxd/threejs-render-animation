ad03a523702debcd4de3648e08ab711ccb25c0b6
Klayver Ximenes <klayver.carmo@dellead.com>
2023-01-31 14:04:10 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Suspense } from 'react'
+
+import { Canvas } from '@react-three/fiber'
+import { Environment } from '@react-three/drei/native'
+
+import ModelYBot from './ModelYBot'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<>
+			<Canvas
+				gl={{ physicallyCorrectLights: true }}
+				camera={{ position: [-6, 0, 16], fov: 36 }}
+			>
+				<color attach="background" args={[0xd9e6ff]} />
+				<ambientLight />
+				<directionalLight intensity={1.1} position={[0, 0, 1]} />
+				<directionalLight intensity={0.8} position={[-6, 2, 2]} />
+				<Suspense>
+					<Environment preset="park" />
+					<ModelYBot />
+				</Suspense>
+			</Canvas>
+		</>
+	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
